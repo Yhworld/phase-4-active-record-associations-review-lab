0f6fcdf6694ffd8c2f1fb6c611004b0f4760cdac
Yahya <yahyahussein670@gmail.com>
2022-09-29 16:07:24 +0300

    Done.

--- db/migrate/20210506153121_create_passengers.rb
+++ db/migrate/20210506153121_create_passengers.rb
@@ -1,6 +1,9 @@
 class CreatePassengers < ActiveRecord::Migration[6.1]
   def change
     create_table :passengers do |t|
+      t.string :name
+      t.string :hometown
+
 
       t.timestamps
     end
